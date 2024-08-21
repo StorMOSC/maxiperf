4f27df8c6b3a20efb89d236108a838243066a1fc
StorMOSC <kakou_de_daoc@hotmail.com>
2024-08-21 21:45:37 +1100

    espace 2

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <title>MaxiPerf - App</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta content="" name="MaxiPerf - App" />
+        <meta content="" name="MaxiPerf - App " />
         <meta content="" name="MaxiPerf" />
         <?php include '_ressources/_inc/css.php'; ?>
         <script src="<?php echo URLHOST.'_ressources/_inc/global/plugins/jquery.min.js'?>"></script>
