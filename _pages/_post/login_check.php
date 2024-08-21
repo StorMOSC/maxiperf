ffb7550cce65c35dc1bacf0fa9c3a5ea72a74682
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2024-08-22 09:26:22 +1100

    page vide

---
+++ _pages/_post/login_check.php
@@ -0,0 +1,28 @@
+<?php
+include("../../_cfg/cfg.php");
+session_start();
+
+    $array = array();
+	$user = new Users($array);
+	$userManager = new UsersManager($bdd);
+
+
+//if(isset($_POST['valider'])){
+
+		$user = $userManager->connectUser($_POST['username'],$_POST['password']);
+//}
+
+if($_COOKIE['connected']=="false"){
+        header('Location: '.URLHOST.'connexion/false');    
+}else{
+        $array = array();
+        $company = new Company($array);
+        $companymanager = new CompaniesManager($bdd);
+        $company = $companymanager->getById($user->getDefaultCompany());
+        
+        setcookie('company', $company->getNameData() , time() + 365*24*3600, '/');
+
+        header('Location: '.URLHOST.$company->getNameData()."/accueil");
+}
+
+?>
---
+++ _pages/accueil.php
@@ -0,0 +1,17 @@
+<html><!--Accueil<br />-->
+<div class="clearfix"></div>
+<!-- END DASHBOARD STATS 1-->
+<div class="row">
+<!--<a href="<?php echo URLHOST;?>devis/creer">Créer Devis</a><br />
+<a href="<?php echo URLHOST;?>dossier/creer">Créer Dossier</a><br />
+<a href="<?php echo URLHOST;?>devis/afficher">Afficher Devis Liste</a><br />
+<a href="<?php echo URLHOST;?>devis/afficher/201902">Afficher Devis Détail</a><br />
+<a href="<?php echo URLHOST;?>dossier/afficher">Afficher Dossier Liste</a><br />
+<a href="<?php echo URLHOST;?>dossier/afficher/111">Afficher Dossier Détail</a><br />-->
+</div>
+<?php 
+$str = "I Tech";
+
+//echo "test : ".$str." => ".strtolower(preg_replace('/[^a-zA-Z0-9-_\.]/','', $str));
+?>
+</html>
