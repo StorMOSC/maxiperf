3ee84cb1960f2cbfcad4eb682ea45826745fa6ba
StorMOSC <kakou_de_daoc@hotmail.com>
2024-08-21 21:29:32 +1100

    test upload

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ License: You must have a valid license purchased only from themeforest(the above
 
     <head>
         <meta charset="utf-8" />
-        <title>StM-Compta</title>
+        <title>MaxiPerf - App</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="" name="MaxiPerf- App" />
