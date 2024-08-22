0e28bb1af6529d21dbd63dd6d244b460d6178c4b
StorMOSC <kakou_de_daoc@hotmail.com>
2024-08-23 00:06:59 +1100

    js

--- _ressources/_inc/js.bottom.php
+++ _ressources/_inc/js.bottom.php
@@ -15,26 +15,25 @@
 <!-- BEGIN PAGE LEVEL PLUGINS -->
 <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-daterangepicker/moment.min.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>
-<?php if($_GET['souscat']=="afficher" || $_GET['souscat']=="creer" || $_GET['souscat']=="modifier" || $_GET["soussouscat"] == "creer" || $_GET["soussouscat"] == "afficher" ){ ?>
-        <script src="https://cdn.datatables.net/plug-ins/1.13.7/sorting/date-eu.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-validation/js/jquery.validate.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/scripts/datatable.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-tabdrop/js/bootstrap-tabdrop.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-confirmation/bootstrap-confirmation.min.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-selectsplitter/bootstrap-selectsplitter.min.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-datepicker/locales/bootstrap-datepicker.fr.min.js" type="text/javascript"></script>
-        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-select/js/bootstrap-select.min.js" type="text/javascript"></script>
-<?php }elseif($_GET['section']=="connexion"){ ?>
+<script src="https://cdn.datatables.net/plug-ins/1.13.7/sorting/date-eu.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-validation/js/jquery.validate.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/scripts/datatable.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-tabdrop/js/bootstrap-tabdrop.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-confirmation/bootstrap-confirmation.min.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-selectsplitter/bootstrap-selectsplitter.min.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-datepicker/locales/bootstrap-datepicker.fr.min.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-select/js/bootstrap-select.min.js" type="text/javascript"></script>
+<?php if($_GET['cat']=="connexion"){ ?>
 <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-validation/js/jquery.validate.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
 <?php }
-        if($_GET['cat']=="societe"){ ?>
+        if($_GET['cat']=="informations"){ ?>
 <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js" type="text/javascript"></script>
 <?php } ?>
 <!-- END PAGE LEVEL PLUGINS -->
@@ -42,7 +41,6 @@
 <script src="<?php echo URLHOST;?>_ressources/_inc/global/scripts/app.min.js" type="text/javascript"></script>
 <!-- END THEME GLOBAL SCRIPTS -->
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
-<?php if($_GET['souscat']=="afficher" || $_GET['souscat']=="creer" || $_GET['souscat']=="modifier" || $_GET["soussouscat"] == "creer" || $_GET["soussouscat"] == "afficher"){ ?>
 <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/ui-modals.min.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/table-datatables-responsive.min.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/form-samples.min.js" type="text/javascript"></script>
@@ -51,12 +49,18 @@
 <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/ui-confirmations.min.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/components-bootstrap-select-splitter.min.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/components-bootstrap-select.min.js" type="text/javascript"></script>
-<?php }elseif($_GET['section']=="connexion"){ ?>
+<?php if($_GET['cat']=="connexion"){ ?>
 <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/login.min.js" type="text/javascript"></script>
 <?php } ?>
 <!-- END PAGE LEVEL SCRIPTS -->
 <!-- BEGIN THEME LAYOUT SCRIPTS -->
+<?php if($_GET['cat']=="connexion"){ ?>
 <script src="<?php echo URLHOST;?>_ressources/_inc/layouts/layout4/scripts/layout.min.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/layouts/layout4/scripts/demo.min.js" type="text/javascript"></script>
 <script src="<?php echo URLHOST;?>_ressources/_inc/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+<?php }else{ ?>
+<script src="<?php echo URLHOST;?>_ressources/_inc/layouts/layout2/scripts/layout.min.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/layouts/layout2/scripts/demo.min.js" type="text/javascript"></script>
+<script src="<?php echo URLHOST;?>_ressources/_inc/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+<?php } ?>
 <!-- END THEME LAYOUT SCRIPTS -->
