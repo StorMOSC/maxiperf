e9a4884be06693aa80e22fbe933fd0c53d12e9ce
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2024-08-22 14:04:58 +1100

    min height

--- index.php
+++ index.php
@@ -92,7 +92,6 @@ if($_GET['section']=="connexion" || $_GET['cat']=="connexion"){
             <!-- BEGIN CONTENT -->
             <div class="page-content-wrapper">
                 <!-- BEGIN CONTENT BODY -->
-
                 <div class="page-content" style="padding-top: 0px !important; min-height: 815px;">
                     <?php if($retour == "errormodif") { ?>
                         <div class="alert alert-danger">
