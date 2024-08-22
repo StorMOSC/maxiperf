8fb8acc5f5b1e8987ba331963b54919641eba8aa
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2024-08-22 11:06:08 +1100

    no user

--- _ressources/top.php
+++ _ressources/top.php
@@ -3,10 +3,10 @@ $nom = $_COOKIE['nom'];
 $prenom = $_COOKIE['prenom'];
 $username = $_COOKIE['username'];
 
-$arraylog = array();
+/*$arraylog = array();
 $userlogged = new Users($arraylog);
 $usermanagerlog = new UsersManager($bdd);
-$userlogged = $usermanagerlog->get($username);
+$userlogged = $usermanagerlog->get($username);*/
 
 ?>
 <!-- BEGIN LOGO -->
