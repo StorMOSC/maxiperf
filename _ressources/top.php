dcace8b090d5b0071f9243b209f79542c9254dcc
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2024-08-22 11:51:13 +1100

    dark top

--- _ressources/top.php
+++ _ressources/top.php
@@ -10,9 +10,9 @@ $userlogged = $usermanagerlog->get($username);*/
 
 ?>
 <!-- BEGIN LOGO -->
-<div class="page-logo">
+<div class="page-logo" style="background:#2f353b !important;">
     <a href="<?php echo URLHOST.'accueil'; ?>">
-        <img src="<?php echo URLHOST; ?>images/logo_transparent_blc.png" alt="logo" class="logo-default" style="height: 45px; margin: 13px 35px 0; " /> </a>
+        <img src="<?php echo URLHOST; ?>images/logo_transparent_blc.png" alt="logo" class="logo-default" style="height: 45px; margin: 15px 0 0; " /> </a>
     <div class="menu-toggler sidebar-toggler">
         <!-- DOC: Remove the above "hide" to enable the sidebar toggler button on header -->
     </div>
@@ -64,19 +64,9 @@ if(count($companymanager)>1){
 <div class="page-top">
     <!-- BEGIN HEADER SEARCH BOX -->
     <!-- DOC: Apply "search-form-expanded" right after the "search-form" class to have half expanded search box -->
-    <form class="search-form" action="page_general_search_2.html" method="GET">
-        <div class="input-group">
-            <input type="text" class="form-control input-sm" placeholder="Search..." name="query">
-            <span class="input-group-btn">
-                <a href="javascript:;" class="btn submit">
-                    <i class="icon-magnifier"></i>
-                </a>
-            </span>
-        </div>
-    </form>
     <!-- END HEADER SEARCH BOX -->
     <!-- BEGIN TOP NAVIGATION MENU -->
-    <div class="top-menu">
+    <div class="top-menu" style="background:#2f353b !important;">
         <ul class="nav navbar-nav pull-right">
             <li class="separator hide"> </li>
             <!-- BEGIN USER LOGIN DROPDOWN -->
