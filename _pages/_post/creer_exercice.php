d925a26bc4943483603b916574eaf85ea528543f
StorMOSC <kakou_de_daoc@hotmail.com>
2024-08-26 21:55:51 +1100

    print_r

--- _pages/_post/creer_exercice.php
+++ _pages/_post/creer_exercice.php
@@ -4,10 +4,13 @@ include("../../_cfg/cfg.php");
 
 if(isset($_POST['creer'])) {
     $name = $_POST['nom_exercice'];
+    $is_active = 1;
+
     echo $name;
+
     $array = array(
         'name' => $name,
-        'isActive' => 1,
+        'isActive' => $is_active
     );
 
 
@@ -17,13 +20,14 @@ if(isset($_POST['creer'])) {
     //echo "OK2 / ".$oldusername;
     $test = $exercicemanager->add($exercice);
     
-
+print_r($array);
 }
 
+/*
 if(is_null($test)){
     header('Location: '.URLHOST."parcours/errormodif");
 }else{
     header('Location: '.URLHOST."parcours/successmodif");
 }
-
+*/
 ?>
