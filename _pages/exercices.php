4a2db7d27b7139f97842d7a0ed4abf1958dcc093
StorMOSC <kakou_de_daoc@hotmail.com>
2024-08-26 23:33:23 +1100

    label série

--- _pages/exercices.php
+++ _pages/exercices.php
@@ -55,8 +55,8 @@ $exercicemanager = $exercicemanager->getList();
                             </div>
                         </div>
                         <div class="form-group col-md-12">
-                            <div id="serie1" class="serie">
-                                <label class="control-label col-md-3">Série 1</label>
+                            <div id="ligneSerie1" class="serie">
+                                <label id="serie1" name="serie[1]" class="control-label col-md-3">Série 1</label>
                                 <div class="col-md-8">
                                     <div class="input-icon">
                                         <input type="number" class="form-control" id="repet1" name="repet[1]" placeholder="Répétition">
@@ -129,14 +129,14 @@ $exercicemanager = $exercicemanager->getList();
         $('#ajout_serie').click(function(){
 
             // get the last DIV which ID starts with ^= "klon"
-            var $div = $('div[id^="serie"]:last').data( "arr", [ 1 ] );
+            var $div = $('div[id^="ligneSerie"]:last').data( "arr", [ 1 ] );
             //var $textarea = $('textarea[id^="descriptionDevis"]:last').data( "txt", [ 1 ] );
             // Read the Number from that DIV's ID (i.e: 3 from "klon3")
             // And increment that number by 1
             var num = parseInt( $div.prop("id").match(/\d+/g), 10 ) +1;
 
             // Clone it and assign the new ID (i.e: from num 4 to ID "klon4")
-            var $klon = $div.clone(true).find(".help-block-error").text("").end().find(".has-error").removeClass("has-error").end().find('input[name^="repet"]:last').prop('name', 'repet['+num+']' ).end().find('input[name^="poids"]:last').prop('name', 'poids['+num+']' ).end().find('select[name^="type"]:last').prop('name', 'type['+num+']' ).end().find('select[id^="type"]:last').prop('id', 'type'+num ).end().find('button[id^="supprSerie"]:last').prop('id', 'supprSerie'+num ).end().find('button[id^="supprSerie"]:last').attr('onclick', 'supprLigneSerie('+num+')' ).end().find('div[id^="divsupprSerie"]:last').prop('id', 'divsupprSerie'+num ).end().find('div[id="divsupprSerie'+num+'"]').css('display','' ).end().find('div[id="divsupprSerie'+num+'"]').css('display','block' ).end().prop('id', 'ligneSerie'+num );
+            var $klon = $div.clone(true).find(".help-block-error").text("").end().find(".has-error").removeClass("has-error").end().find('label[name^="serie"]:last').prop('name', 'serie['+num+']' ).text("Série "+num).end().find('input[name^="repet"]:last').prop('name', 'repet['+num+']' ).end().find('input[name^="poids"]:last').prop('name', 'poids['+num+']' ).end().find('select[name^="type"]:last').prop('name', 'type['+num+']' ).end().find('select[id^="type"]:last').prop('id', 'type'+num ).end().find('button[id^="supprSerie"]:last').prop('id', 'supprSerie'+num ).end().find('button[id^="supprSerie"]:last').attr('onclick', 'supprLigneSerie('+num+')' ).end().find('div[id^="divsupprSerie"]:last').prop('id', 'divsupprSerie'+num ).end().find('div[id="divsupprSerie'+num+'"]').css('display','' ).end().find('div[id="divsupprSerie'+num+'"]').css('display','block' ).end().prop('id', 'ligneSerie'+num );
 
             // Finally insert $klon wherever you want
             $("div[id*='divsupprSerie']").css('display','' );
