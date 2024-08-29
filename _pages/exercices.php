daf6a72f8d7c0ff5a29eed6e8cf0d5b4359a0b3f
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2024-08-29 13:27:39 +1100

    ["series"]

--- _pages/exercices.php
+++ _pages/exercices.php
@@ -262,7 +262,7 @@ $exercicemanager = $exercicemanager->getListAllExercices();
 
                     //jsonParcours = JSON.parse(jsonParcours);
                     jsonParcours["exercices"]["series"] = jsonSerie;
-                    jsonSerie_aff = jsonSerie_aff+" jsonSerie : "+JSON.stringify(jsonParcours["exercices"])+" - ";
+                    jsonSerie_aff = jsonSerie_aff+" jsonSerie : "+JSON.stringify(jsonParcours["exercices"]["series"])+" - ";
                     alert("5");
                     //document.getElementById("json_result").innerHTML = "jsonSerie : "+JSON.stringify(jsonParcours);
 
