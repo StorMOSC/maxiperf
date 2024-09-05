b9f9cd77a91073fe626c8671bbfb788adaa9878a
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2024-09-05 11:49:28 +1100

    replaceWith

--- _pages/exercices.php
+++ _pages/exercices.php
@@ -247,7 +247,7 @@ $exercicemanager = $exercicemanager->getListAllExercices();
 
             if(jsonParcours.hasOwnProperty('nom')){
 
-                document.getElementById("json_result").innerHTML = "";
+                document.getElementById("json_result").replaceWith("");
 
                 nom_exercice_input = document.getElementById("exercice");
                 nom_exercice = nom_exercice_input.value;
