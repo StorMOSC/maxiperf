659d8a4d168581e3f88f951b01aeaca6119f2b8c
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2024-09-12 11:32:49 +1100

    test sortie

--- _pages/exercices.php
+++ _pages/exercices.php
@@ -382,10 +382,6 @@ $exercicemanager = $exercicemanager->getListAllExercices();
                     });
 
                     //jsonParcours.exercices[key].nom_exo
-                    tabParcours += '<input type="text" class="form-control" style="visibility : hidden;" id="parcours_nom" name="parcours_nom" value="'+jsonParcours.nom+'">';
-                    tabParcours += '<input type="text" class="form-control" style="visibility : hidden;" id="parcours_date" name="parcours_date" value="'+jsonParcours.date+'">';
-                    tabParcours += '<textarea class="form-control" style="visibility : hidden;" id="parcours_commentaire" name="parcours_commentaire">'+jsonParcours.commentaire+'</textarea>';
-                    tabParcours += '<input type="text" class="form-control" style="visibility : hidden;" id="parcours_exercice_id'+num_input+'" name="parcours_exercice_id['+num_input+']" value="'+jsonParcours.exercices[key].nom_exo+'">';
 
                     tabParcours += '<div class=\"panel-heading\">'+nom_exercice_string+'</div>';
 
@@ -414,6 +410,10 @@ $exercicemanager = $exercicemanager->getListAllExercices();
 
             }
             tabParcours += '</div>';
+            tabParcours += '<input type="text" class="form-control" style="visibility : hidden;" id="parcours_nom" name="parcours_nom" value="'+jsonParcours.nom+'">';
+                    tabParcours += '<input type="text" class="form-control" style="visibility : hidden;" id="parcours_date" name="parcours_date" value="'+jsonParcours.date+'">';
+                    tabParcours += '<textarea class="form-control" style="visibility : hidden;" id="parcours_commentaire" name="parcours_commentaire">'+jsonParcours.commentaire+'</textarea>';
+                    tabParcours += '<input type="text" class="form-control" style="visibility : hidden;" id="parcours_exercice_id'+num_input+'" name="parcours_exercice_id['+num_input+']" value="'+jsonParcours.exercices[key].nom_exo+'">';
 
             document.getElementById("parcours_div").style.visibility = "visible";
             //alert(jsonParcours.nom);
